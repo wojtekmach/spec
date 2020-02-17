52e62fa5f98d4145bf7b7d40224b8b1bba2d2eb7
Wojtek Mach <wojtek@wojtekmach.pl>
2020-02-18 00:29:38 +0100

    Conform predicates

--- lib/spec.ex
+++ lib/spec.ex
@@ -1,2 +1,40 @@
 defmodule Spec do
+  def conform(value, spec) do
+    Spec.Spec.conform(spec, value)
+  end
+
+  def conform!(value, spec) do
+    case conform(value, spec) do
+      {:ok, value} ->
+        value
+
+      {:error, exception} ->
+        raise exception
+    end
+  end
+end
+
+defprotocol Spec.Spec do
+  def conform(spec, value)
+end
+
+defmodule Spec.Error do
+  defexception [:spec, :value]
+
+  @impl true
+  def message(%{spec: spec, value: value}) do
+    "cannot conform `#{inspect(value)}` to `#{inspect(spec)}`"
+  end
+end
+
+defimpl Spec.Spec, for: Function do
+  def conform(spec, value) when is_function(spec, 1) do
+    case spec.(value) do
+      true ->
+        {:ok, value}
+
+      false ->
+        {:error, %Spec.Error{spec: spec, value: value}}
+    end
+  end
 end
--- test/spec_test.exs
+++ test/spec_test.exs
@@ -1,3 +1,13 @@
 defmodule SpecTest do
   use ExUnit.Case, async: true
+
+  describe "conform/2" do
+    test "predicates" do
+      assert Spec.conform!(1, &is_integer/1) == 1
+
+      assert_raise Spec.Error, "cannot conform `:one` to `&:erlang.is_integer/1`", fn ->
+        Spec.conform!(:one, &is_integer/1)
+      end
+    end
+  end
 end
